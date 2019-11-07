9c467e6f7fbf258d35052993a74ff38d5872f199
songmin-gif <55427684+songmin-gif@users.noreply.github.com>
2019-11-07 16:10:37 +0900

    10pro

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void)
+{
+		int i;
+		int grade[5];
+		int average=0;
+		int *gptr;
+		
+		for(i=0;i<5;i++)
+		{
+			printf("grade[%i]=",i);
+			scanf("%d",&grade[i]);
+		}
+		gptr= grade;
+		for(i=0;i<5;i++){
+		printf("grade[%d]=%d\n",i,*(gptr+i));
+		average+= *(gptr+i);
+	}
+	printf("\n -- average: %i\n",average/5);
+	
+	return 0;
+}
